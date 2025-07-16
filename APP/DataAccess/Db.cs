12456f873132e727ccfe70a9380e0271cfe18932
Yasinus <Yasinus@DESKTOP-29IUMJH>
2025-07-16 03:29:12 +0300

    bugfix

--- APP/DataAccess/Db.cs
+++ APP/DataAccess/Db.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace APP.DataAccess
 {
-    internal class Db
+    public class Db : DbContext
     {
+        public DbSet<Product> Products { get; set; }
     }
 }
