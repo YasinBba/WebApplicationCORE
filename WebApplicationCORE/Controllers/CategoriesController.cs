743f38c87f6e73df50ee84751d0040bd5eb4bded
Yasinus <Yasinus@DESKTOP-29IUMJH>
2025-08-01 03:34:25 +0300

    Catogories listeleme tamamlandı

---
+++ WebApplicationCORE/Controllers/CategoriesController.cs
@@ -0,0 +1,38 @@
+﻿using APP.Business.Models;
+using APP.DataAccess;
+using CORE.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationCORE.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly Service<Category, CategoryRequest, CategoryResponse> _service;
+
+        public CategoriesController(Service<Category, CategoryRequest, CategoryResponse> service)
+        {
+            _service = service;
+        }
+
+        // GET: Categories/Index
+        // GET: Categories
+        public IActionResult Index()
+        {
+            var result = _service.GetList();
+
+            // Way 1:
+            //ViewData["Message"] = result.Message;
+            // Way 2:
+            ViewBag.Message = result.Message;
+
+            return View(result.Data);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var result = _service.GetItem(id);
+            ViewBag.Message = result.Message;
+            return View(result.Data);
+        }
+    }
+}
